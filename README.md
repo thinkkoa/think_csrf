b40635c4df7dd73021d9e8c100a553d719e9eeb5
yanglin <richenlin@gmail.com>
2017-08-09 16:28:45 +0800

    init

--- README.md
+++ README.md
@@ -1,2 +1,48 @@
-# think_csrf
+# 介绍
+-----
+
+[![npm version](https://badge.fury.io/js/think_csrf.svg)](https://badge.fury.io/js/think_csrf)
+[![Dependency Status](https://david-dm.org/thinkkoa/think_csrf.svg)](https://david-dm.org/thinkkoa/think_csrf)
+
 CSRF for ThinkKoa.
+
+# 安装
+-----
+
+```
+npm i think_csrf
+```
+
+# 使用
+-----
+注意: think\_csrf 中间件依赖 think\_chache 中间件,在使用此中间件之前,请安装配置cache中间件
+
+1、项目中增加中间件 middleware/csrf.js
+```
+module.exports = require('think_csrf');
+```
+
+2、项目中间件配置 config/middleware.js:
+```
+list: [..., 'csrf'], //加载的中间件列表
+config: { //中间件配置
+    ...,
+    csrf: {
+        session_name: 'csrf_token', // session存储key
+        form_name: '_csrf', // csrf传递参数 key
+        header_name: 'x-csrf-token', // csrf使用header传递参数 key
+        errno: 403, // 错误码，未通过csrf检测抛出
+        errmsg: 'invalid csrf token' // 错误信息，未通过csrf检测抛出
+    }
+}
+```
+
+3、获取CSRF值：
+
+```
+// @controller
+let csrfVal = this.ctx.csrf;
+
+// @middleware
+let csrfVal = ctx.csrf;
+```
---
+++ index.js
@@ -0,0 +1,76 @@
+/**
+ *
+ * @author     chenjp
+ * @copyright  Copyright (c) 2017 - <chenjp(at)yunheit.com>
+ * @license    MIT
+ * @version    6/8/17
+ */
+const lib = require('think_lib');
+const crypto = require('crypto');
+
+/**
+ * default options
+ */
+const defaultOptions = {
+    session_name: 'csrf_token', // session存储key
+    form_name: '_csrf', // csrf传递参数 key
+    header_name: 'x-csrf-token', // csrf使用header传递参数 key
+    errno: 403, // 错误码，未通过csrf检测抛出
+    errmsg: 'invalid csrf token' // 错误信息，未通过csrf检测抛出
+};
+
+/**
+ * 生成uid
+ * @param length
+ * @returns {string}
+ */
+const cookieUid = function (length) {
+    let str = crypto.randomBytes(Math.ceil(length * 0.75)).toString('base64').slice(0, length);
+    return str.replace(/[\+\/]/g, '_');
+};
+
+/**
+ * 
+ * 
+ * @param {any} ctx 
+ * @param {any} { session_name } 
+ * @returns 
+ */
+const ensureCsrfToken = function (ctx, session_name) {
+    const token = cookieUid(32);
+    return ctx.session(session_name).then(value => value ? Promise.resolve(value) : ctx.session(session_name, token).then(() => token));
+};
+
+/**
+ * 
+ * 
+ * @param {any} ctx 
+ * @param {any} options 
+ * @returns 
+ */
+const checkCsrf = function (ctx, options) {
+    return ctx.session(options.session_name).then(value => {
+        if (!value) {
+            ctx.throw(options.errno, options.errmsg);
+        }
+        const token = ctx.query[options.form_name] || (ctx.request.body.post && ctx.request.body.post[options.form_name]) || ctx.get(options.header_name);
+        if (token !== value) {
+            ctx.throw(options.errno, options.errmsg);
+        }
+    });
+};
+
+module.exports = function (options) {
+    options = options ? lib.extend(defaultOptions, options, true) : defaultOptions;
+    return function (ctx, next) {
+        if (!ctx.session) {
+            ctx.throw(500, 'please install think_session middleware');
+        }
+
+        if (ctx.method === 'GET' || ctx.method === 'HEAD' || ctx.method === 'OPTIONS' || ctx.method === 'TRACE') {
+            return ensureCsrfToken(ctx, options).then(value => lib.define(ctx, 'csrf', value)).then(() => next());
+        }
+
+        return checkCsrf(ctx, options).then(() => next());
+    };
+};
